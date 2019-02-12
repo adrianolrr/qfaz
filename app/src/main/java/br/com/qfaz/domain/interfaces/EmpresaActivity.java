703e1d05ca3b9dc6c9c0c14065584d39d797e7dd
adrianolrr <adriano.lrr@gmail.com>
2019-02-12 17:48:12 -0200

    v5

---
+++ app/src/main/java/br/com/qfaz/domain/interfaces/EmpresaActivity.java
@@ -0,0 +1,15 @@
+package br.com.qfaz.domain.interfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.qfaz.R;
+
+public class EmpresaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa);
+    }
+}
